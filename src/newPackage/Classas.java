9c458a7255a6f176159f501754b4f942bc3174c3
Luiz-om <luizfernando_martins@hotmail.com>
2018-12-20 18:13:57 -0300

    criação classe Classas

--- src/newPackage/Classas.java
+++ src/newPackage/Classas.java
@@ -3,7 +3,10 @@ package newPackage;
 public class Classas {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int idade=10;
+		
+		System.out.println(idade);
 
 	}
 
