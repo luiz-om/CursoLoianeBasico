f8edb4d381c92fcb8b37cc25cec8a281dfc571c6
luiz-om <luizfernando_martins@hotmail.com>
2019-01-06 17:13:22 -0300

    aula19 inicio

---
+++ src/com/agro/aula19/Arrays.java
@@ -0,0 +1,15 @@
+package com.agro.aula19;
+
+import java.util.Scanner;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		
+
+		Scanner scan= new Scanner(System.in);
+		
+		
+	}
+
+}
